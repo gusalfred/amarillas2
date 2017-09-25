77ae6bf2032a6bfa302895f74b6e7a0c046887b1
Gustavo peña <gusalfred922@gmail.com>
2017-09-25 13:59:23 -0400

    archivos faltantes
    agregar los archivo ignorados por git

---
+++ vendor/laravel/framework/src/Illuminate/Pagination/resources/views/default.blade.php
@@ -0,0 +1,36 @@
+@if ($paginator->hasPages())
+    <div class="btn-group btn-group-justified">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <a class="disabled btn btn-default"><span class="fa fa-chevron-left"></span></a>
+        @else
+            <a  class="btn btn-default" href="{{ $paginator->previousPageUrl() }}" rel="prev"><i class="fa fa-chevron-left"></i></a>
+        @endif
+
+        {{-- Pagination Elements --}}
+        @foreach ($elements as $element)
+            {{-- "Three Dots" Separator --}}
+            @if (is_string($element))
+                <a class="btn btn-default disabled"><span>{{ $element }}</span></a>
+            @endif
+
+            {{-- Array Of Links --}}
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                        <a class="btn btn-primary"><span>{{ $page }}</span></a>
+                    @else
+                        <a class="btn btn-default" href="{{ $url }}">{{ $page }}</a>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <a  class="btn btn-default" href="{{ $paginator->nextPageUrl() }}" rel="next"><i class="fa fa-chevron-right"></i></a>
+        @else
+            <a class="btn btn-default disabled"><span  class="fa fa-chevron-right"></span></a>
+        @endif
+    </div>
+@endif
